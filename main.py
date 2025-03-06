79920c5a649eec539ceef391e46422d259e35fd1
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-03-06 17:21:37 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os 
+import time
+
+
+def print_Options():
+    print("agenda de contactos")
+    print('*' * 50)
+    print('Selecciona una opción:')
+    print('[C]rear contacto')
+    print('[L]istado de contactos')
+    print('[M]odificar contacto')
+    print('[E]liminar contacto')
+    print('[B]uscar contacto')
+    print('[S]ALIR')
+
+
+def run():
+    print_Options()
+
+
+
+
+    
