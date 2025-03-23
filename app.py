cb8ce7a0ae4044f7c9a270fc3020e51a1463e6f0
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-03-23 14:11:12 -0600

    pruebamerge

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "admin" and password == "admin123":
+            return redirect(url_for("index"))
+        else:
+            return "Login failed. Please check your credentials."
+
+    return render_template("login.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-	
+    app.run(debug=True)
